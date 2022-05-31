a00995cae240ca8483d43d1a4053e879e4496399
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2022-05-31 19:02:50 -0400

    Updated algorithm implementation — now works! 😁

--- src/affirmative_sampling/algorithm.py
+++ src/affirmative_sampling/algorithm.py
@@ -1,15 +1,23 @@
 
+import math
+import typing
+
 import randomhash
 
-def affirmativeSampling(k, tokens):
+
+def affirmativeSampling(tokens: typing.List[str], k: int):
     
-    min_hash = None
-    
-    # fill S with the first k distinct elements (and hash values)
+    # initialize the sample
     
     sample_core = set()
     sample_xtra = set()
     sample_freqs = {}
+    
+    # INITIAL PART
+    # ============
+
+    # fill S with the first k distinct elements (and hash values)
+    
     idx = 0
     while len(sample_core) < k and idx < len(tokens):
         z = tokens[idx]
@@ -20,61 +28,79 @@ def affirmativeSampling(k, tokens):
         # update frequency
         sample_freqs[z] = sample_freqs.get(z, 0) + 1
     
-    # compute current minimum
+    # compute current minimum and kth largest element of the sample
     
     _g = lambda z: (randomhash.hash(z), z)
-    min_hash, z_min_hash = min(map(_g, sample_core))
-    max_hash, z_max_hash = max(map(_g, sample_core))
-    
-    # main loop
+    min_hash, z_min_hash = min(map(_g, sample_core))  # sample_core here because sample_xtra is empty
+    kth_hash, z_kth_hash = min(map(_g, sample_core))
+
+    # MAIN LOOP
+    # =========
     
     for z in tokens[idx:]:
         
-        # if z in the sample
+        # (A) if z in the sample
         if z in sample_core or z in sample_xtra:
-            # updating frequency
+            # update frequency
             # (should also update any other stat of z)
             sample_freqs[z] = sample_freqs.get(z, 0) + 1
             continue
         
-        # z is not in the sample, so compute y=hash(z)
+        # (B) z is not in the sample, so compute y=hash(z)
         
         y = randomhash.hash(z)
-        
-        #   sample -----------\
-        #                     |
-        #          min_hash   |   k-th largest hash value
-        #             |       |            |
-        #             v       v            v
-        # 0 --------- [[<elem_1>, ..., <elem_k>]] --------- 1
-        #   DISCARD  |         REPLACE          | EXPAND
-        #
-        
-        # if y is smaller than the min hash value in S
+ 
+        # (B.1) if y is smaller than the min hash value in S
         if y < min_hash:
-            # DISCARD(/IGNORE)
+            # DISCARD = ignore the element entirely
             continue
-        
-        # if y is larger than the k-th largest hash value in S
-        # (if y is larger than any hash value in the sample)
-        elif y > max_hash:
-            # EXPAND
-            sample_xtra.add(z)
-        
-        # otherwise z replaces the element z* with min. hash value
-        else:
-            # REPLACE
-            # remove z*
-            sample_core.remove(z_min_hash)
-            sample_freqs.remove(z_min_hash)
-            
-            # add z
+
+        # (B.2) if y is larger than the k-th largest hash value in S
+        # y is a new k-record, needs not to be larger than the largest in the sample!
+        elif y > kth_hash:
+            # EXPAND = the total size sample grows by one
+            # (but sample_core remains at size k)
+
+            # add z to S
             sample_core.add(z)
             sample_freqs[z] = 1
+
+            # move z_kth_hash from core to xtra
+            sample_xtra.add(z_kth_hash)
+            sample_core.remove(z_kth_hash)
+
+            min_hash, z_min_hash = min(map(_g, sample_xtra))
+            kth_hash, z_kth_hash = min(map(_g, sample_core))
             
-            # update min/max
-            min_hash, z_min_hash = min(map(_g, sample_core))
-            max_hash, z_max_hash = max(map(_g, sample_core))
+        # (B.3) otherwise z replaces the element z* with min. hash value
+        # sample_xtra must contain z_min_hash otherwise we have a contradiction
+        else:
+            # REPLACE = the size of the sample does not change but we
+            # make sure to keep the largest elements
+
+            # remove the element with min. hash value
+            sample_xtra.remove(z_min_hash)
+            del sample_freqs[z_min_hash]
+
+            # and replace by the current element z
+            sample_xtra.add(z)
+            sample_freqs[z] = 1
+
+            # only sample_xtra has changed, no need to update kth_hash
+            min_hash, z_min_hash = min(map(_g, sample_xtra))
             
     
-    return sample_freqs, len(sample_core), len(sample_xtra)
+    # compute estimators
+    recordinality = k*(1 + 1/k)**(len(sample_freqs)-k+1)
+    af_kmv = (len(sample_freqs)-1)/(1-randomhash.int_to_real(min_hash))
+    expected_sample_size = k*math.log(n/k) + k
+    
+    return {
+        "cardinality": {
+            "recordinality": recordinality,
+            "af-kmv": af_kmv,
+        },
+        "expectedSampleSize": expected_sample_size,
+        "sampleSize": len(sample_freqs),
+        "sample": sample_freqs
+    }
